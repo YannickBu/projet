486a6f1ac681fee150bfee5118a777e9b5fde213
bahbahsara <bahbahsara@mbp-de-sara>
2014-12-07 15:46:33 +0100

    changer dans le metier supprimerReservation

--- Projet/src/metier/SupprimerReservation.java
+++ Projet/src/metier/SupprimerReservation.java
@@ -1,27 +1,22 @@
 package metier;
 
-import java.util.Date;
 
 import donnee.Client;
 import donnee.Reservation;
-import donnee.Salle;
 import fabrique.FabClient;
 import fabrique.FabReservation;
 
 public class SupprimerReservation {
 	
-	public void supprimerResrevation (Integer idres,Client c,Salle s,Date d,int plage, Integer idClient) {
-		FabClient client = FabClient.getInstance();
+	public void supprimerResrevation (Integer idres, Integer idClient) {
+		FabClient fabClient = FabClient.getInstance();
 		FabReservation reservation = FabReservation.getInstance();
-		if (client.rechercher(idClient) != null && idres != null){
-			Reservation res = reservation.rechercher(idres);
-			if ( res != null) {
-				Client cl = client.rechercher(idClient);
-				cl.getListReservation().remove(res);
-				reservation.rechercher(idres).setClient(null);	
-			}
+		Reservation res = reservation.rechercher(idres);
+		Client client = fabClient.rechercher(idClient);
+		
+		if (client != null && res != null){
+			reservation.supprimer(idres);	
 		}
-			
 	}
 
 }
