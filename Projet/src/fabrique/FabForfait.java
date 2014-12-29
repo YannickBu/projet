5fa32740d7b5fcfb3ca939ffc8c1e36289ea8a09
bahbahsara <bahbahsara@mbp-de-sara>
2014-12-29 20:19:39 +0100

    enlever des import qui ne servent a rien

--- Projet/src/fabrique/FabForfait.java
+++ Projet/src/fabrique/FabForfait.java
@@ -4,11 +4,11 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Date;
+
 import java.util.List;
 
 import donnee.Forfait;
-import donnee.ForfaitClient;
+
 
 
 public class FabForfait {
