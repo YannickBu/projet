f3688cf203b9df01aa3adf183b4e1220aef417a8
bahbahsara <bahbahsara@mbp-de-sara>
2014-12-29 20:59:30 +0100

    confimerReservation fini reste a verifier

--- Projet/src/metier/ConfirmerReservation.java
+++ Projet/src/metier/ConfirmerReservation.java
@@ -1,11 +1,17 @@
 package metier;
 
+import java.text.SimpleDateFormat;
+
 import donnee.Client;
 import donnee.ForfaitClient;
 import donnee.Reservation;
+import donnee.Salle;
+import donnee.TypeSalle;
 import fabrique.FabClient;
 import fabrique.FabForfaitClient;
 import fabrique.FabReservation;
+import fabrique.FabSalle;
+import fabrique.FabTypeSalle;
 
 
 public class ConfirmerReservation {
@@ -29,7 +35,7 @@ public class ConfirmerReservation {
 			}
 		}
 		
-		//TODO a revoir 
+		//TODO a verifier par toi Yannick :p 
 		// Cette methode a utiliser si le client veut utiliser ses points de fidelite et/ou son forfait
 		/**
 		 * payer une reservation avec les points de fidelite et/ou forfait
@@ -44,7 +50,81 @@ public class ConfirmerReservation {
 			Client c = client.rechercher(idClient);
 			Reservation res = reservation.rechercher(idReservation);
 			ForfaitClient fc = forfaitClient.rechercher(idForfaitClient);
+			FabTypeSalle typeSalle = FabTypeSalle.getInstance();
+			TypeSalle ts = typeSalle.rechercher(fc.getTypeSalle().getIdTypeSalle());
+			FabSalle salle = FabSalle.getInstance();
+			Salle s = salle.rechercherParType(ts.toString());
 			
+			SimpleDateFormat formatter = new SimpleDateFormat("HH");
+			int heure = Integer.parseInt(formatter.format(res.getDate()));
+			
+			// Le paiement de 20h et plus
+			if (heure >= 20 && c!= null && res != null){
+				int plage = res.getPlage();
+				double prix=0;
+				while (plage != 0){ // pour le calcul du prix
+					if ((plage%2) == 0){
+						prix = s.getPrixPlage2h()*(2/100) + s.getPrixPlage2h();
+					}
+					else {
+						prix = s.getPrixPlage1h()*(2/100) + s.getPrixPlage1h();
+					}
+				}
+				if (plage == 2) { // pour le paiment avec le forfait ou point de fidelite la difference qui reste en cash
+					if (c.getPointsFidelite() >= 150) {
+						int pointFidelite = c.getPointsFidelite() - 150;
+						c.setPointsFidelite(pointFidelite);
+						prix = prix - s.getPrixPlage2h();
+					} else {
+						if (fc.getTempsRestant() >= 2){
+							int temps = fc.getTempsRestant();
+							temps = temps - 2;
+							fc.setTempsRestant(temps);
+							prix = prix - s.getPrixPlage2h();
+						}
+					}
+				}
+				if (plage > 2) {
+					while (c.getPointsFidelite() >= 150 && plage != 0) {
+						int pointFidelite = c.getPointsFidelite() - 150;
+						c.setPointsFidelite(pointFidelite);
+						plage = plage - 2;
+						prix = prix - s.getPrixPlage2h();
+					}
+					if (plage >= 2) {
+						while (fc.getTempsRestant() >= 1 && plage != 0){
+							int temps = fc.getTempsRestant();
+							temps = temps - 1;
+							fc.setTempsRestant(temps);
+							plage = plage -1;
+							prix = prix - s.getPrixPlage1h();
+						}
+					}
+					if (plage >= 1 || plage == 0 && prix >=0 ){ // Le client paye cash la difference
+						res.setEstPaye(true);
+						c.ajoutPointFidelite(res.getPlage());
+					}
+				}		
+				if (plage == 1) {
+					if (fc.getTempsRestant() >= 1){
+						int temps = fc.getTempsRestant();
+						temps = temps - 1;
+						fc.setTempsRestant(temps);
+						prix = prix - s.getPrixPlage1h();
+					}
+					else {
+						res.setEstPaye(true);// le client paye par cash la reservation
+						c.ajoutPointFidelite(res.getPlage());
+					}
+					res.setEstPaye(true); // Le client paye cash la difference qui reste
+					c.ajoutPointFidelite(res.getPlage());
+				}
+			}
+				
+			
+			
+			
+			// Le paiement avant 20h 
 			if (c != null && res != null) {
 				if (res.getPlage() == 2) {
 					if (c.getPointsFidelite() >= 150) {
@@ -63,27 +143,29 @@ public class ConfirmerReservation {
 					}
 				}
 				if (res.getPlage() > 2) {
-					if (c.getPointsFidelite() >= 150) {
+					int plage = res.getPlage();
+					while (c.getPointsFidelite() >= 150 && plage != 0) {
 						int pointFidelite = c.getPointsFidelite() - 150;
 						c.setPointsFidelite(pointFidelite);
-						int plage = res.getPlage() - 2;
-						if (fc.getTempsRestant() >= 2){
-							int temps = fc.getTempsRestant();
-							temps = temps - 2;
-							fc.setTempsRestant(temps);
-							plage = res.getPlage() -2;
-							if (plage == 0 ){
-								res.setEstPaye(true);
-								c.ajoutPointFidelite(res.getPlage());
-							}
+						plage = plage - 2;
 						}
-						
+					if (plage >= 2) {
+						while (fc.getTempsRestant() >= 1 && plage != 0){
+							int temps = fc.getTempsRestant();
+							temps = temps - 1;
+							fc.setTempsRestant(temps);
+							plage = plage -1;
+						}
 					}
-				}
+					if (plage >= 1 || plage == 0 ){
+						res.setEstPaye(true);
+						c.ajoutPointFidelite(res.getPlage());
+					}
+				}		
 				if (res.getPlage() == 1) {
-					if (fc.getTempsRestant() >= 2){
+					if (fc.getTempsRestant() >= 1){
 						int temps = fc.getTempsRestant();
-						temps = temps - 2;
+						temps = temps - 1;
 						fc.setTempsRestant(temps);
 						res.setEstPaye(true);
 						c.ajoutPointFidelite(res.getPlage());
@@ -96,4 +178,5 @@ public class ConfirmerReservation {
 			}
 		}	
 
+		
 }
