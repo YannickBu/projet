9d69122071f83c40c7a95ce42459c3a350582c85
bahbahsara <bahbahsara@mbp-de-sara>
2014-12-31 16:04:50 +0100

    JavaDoc

--- Projet/src/metier/SupprimerReservation.java
+++ Projet/src/metier/SupprimerReservation.java
@@ -10,13 +10,16 @@ public class SupprimerReservation {
 	/**
 	 * Methode qui permet de supprimer une reservation
 	 * @param idres
-	 * @param idClient
 	 */
 	public void supprimerReservation (Integer idres) {
 		FabReservation.getInstance().supprimer(idres);	
 	}
 	
-	
+	/**
+	 * Methode qui permet de supprimer une reservation par date et salle
+	 * @param date
+	 * @param idSalle
+	 */
 	public void supprimerReservation (Date date, Integer idSalle){
 		FabReservation.getInstance().supprimer(date, idSalle);
 	}
